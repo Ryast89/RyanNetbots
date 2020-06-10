d48b5823b530bf9578b345bb910b967e4a11f95a
Ryan Stroebel <36288677+Ryast89@users.noreply.github.com>
2020-06-10 16:56:50 -0700

    Update Ryanfinal.py

--- Ryanfinal.py
+++ Ryanfinal.py
@@ -71,10 +71,10 @@ def play(botSocket, srvConf):
                  
             timer = timer - 1
             if timer <= 0:
-                timer = 20
+                timer = 25
                 currentDirection = currentDirection + math.pi*0.25
                 botSocket.sendRecvMessage({'type': 'setDirectionRequest', 'requestedDirection': nbmath.normalizeAngle(currentDirection)})
-                botSocket.sendRecvMessage({'type': 'setSpeedRequest', 'requestedSpeed': 85})
+                botSocket.sendRecvMessage({'type': 'setSpeedRequest', 'requestedSpeed': 80})
             if currentMode == "wait":
                 
                 getCanonReply = botSocket.sendRecvMessage({'type': 'getCanonRequest'})
